6d1edfe550d4d349cedec671ca4a223082bb0020
Alice-Rez <reznickova.alice@seznam.cz>
2021-04-07 00:34:28 +0200

    created dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { AppService } from './home/app.service';
 
 @NgModule({
   declarations: [AppComponent],
