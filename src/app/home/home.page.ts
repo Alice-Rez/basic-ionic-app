bd449878f19e52beae31fd28e8ecde16cdbcfe2c
Alice-Rez <reznickova.alice@seznam.cz>
2021-04-06 23:44:38 +0200

    changing bgr when wrong combination implemented

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
 export class HomePage {
   public name: string = '';
   public password: string = '';
+  public wrongCombination: boolean = false;
 
   constructor() {}
+
+  public login(): void {
+    if (this.name.split('').reverse().join('') !== this.password) {
+      this.wrongCombination = true;
+    }
+  }
+
+  public hideWarning(): void {
+    this.wrongCombination = false;
+  }
 }
